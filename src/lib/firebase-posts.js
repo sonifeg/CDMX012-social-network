3dd1ec5cd42b253987de46139c9a8c60b57eef10
sonifeg <l.d.i-sfg@hotmail.com>
2022-04-07 13:22:31 -0500

    pagina posts muestra datos en consola

--- src/lib/firebase-posts.js
+++ src/lib/firebase-posts.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable import/named */
 import {
   collection,
@@ -28,27 +29,20 @@ export const savePost = (post, date) => {
       email,
       photo,
     });
-  } else {
-    // No user is signed in.
   }
 };
 
 export const getPost = () => {
   const posts = collection(db, 'posts');
-  const docPost = getDocs(posts);
-  docPost.forEach((element) => {
-    const postNews = document.getElementById('sectionContainerPost');
-    if (element.posts.exists()) {
-      const postSection = document.createElement('section');
-      postSection.className = 'sectionContainerPost';
-      const userPostValue = posts.username;
-      const postValue = posts.post;
-      const username = document.createElement('p');
-      const postText = document.createElement('p');
-      username.textContent(userPostValue);
-      postText.textContent(postValue);
-      postNews.append(username, postText);
-    }
-    return postNews;
-  });
+  getDocs(posts)
+    .then((snapshot) => {
+      let postsInfo = [];
+      snapshot.docs.forEach((doc) => {
+        postsInfo.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(postsInfo);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
